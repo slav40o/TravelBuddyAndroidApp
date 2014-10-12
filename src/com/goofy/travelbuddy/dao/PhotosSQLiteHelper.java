807b0e610cc2b6daa612228bed1bdc089a6cf173
bboyadzhiev <bboyadzhiev@gmail.com>
2014-10-13 00:48:04 +0300

    Places and Photos DataSource and SQL helpers

---
+++ src/com/goofy/travelbuddy/dao/PhotosSQLiteHelper.java
@@ -0,0 +1,37 @@
+package com.goofy.travelbuddy.dao;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class PhotosSQLiteHelper  extends SQLiteOpenHelper {
+	public static final String TABLE_PHOTOS = "places";
+	public static final String COLUMN_ID = "_id";
+	public static final String COLUMN_NAME = "name";
+	public static final String COLUMN_IMAGE = "image";
+	public static final String COLUMN_USERID = "userid";
+	public static final String COLUMN_PLACEID= "placeid"; // real
+	private static final String DATABASE_NAME = "travelbuddy.db";
+	private static final int DATABASE_VERSION = 1;
+	
+	 private static final String DATABASE_CREATE = "create table "
+	            + TABLE_PHOTOS + "(" + COLUMN_ID + " integer primary key autoincrement, " 
+	            + COLUMN_IMAGE + " BLOB not null" 
+	            + COLUMN_USERID + " integer not null" 
+	            + COLUMN_PLACEID + " integer not null" 
+	            		+ ");";
+	public PhotosSQLiteHelper (Context context){
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(DATABASE_CREATE);
+		
+	}
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_PHOTOS);
+        onCreate(db);
+		
+	}
+}
---
+++ src/com/goofy/travelbuddy/dao/PlacesSQLiteHelper.java
@@ -0,0 +1,45 @@
+package com.goofy.travelbuddy.dao;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class PlacesSQLiteHelper extends SQLiteOpenHelper {
+
+	public static final String TABLE_PLACES = "places";
+	public static final String COLUMN_ID = "_id";
+	public static final String COLUMN_TITLE = "title";
+	public static final String COLUMN_DESCRIPTION = "decription";
+	public static final String COLUMN_COUNTRY = "country";
+	public static final String COLUMN_LAT= "lat"; // real
+	public static final String COLUMN_LON = "lon"; // real
+	public static final String COLUMN_LAST_VISIT = "lastvisited"; // Text as ISO8601 strings ("YYYY-MM-DD HH:MM:SS.SSS").
+	private static final String DATABASE_NAME = "travelbuddy.db";
+	private static final int DATABASE_VERSION = 1;
+
+	  // Database creation SQLite statement
+    private static final String DATABASE_CREATE = "create table "
+            + TABLE_PLACES + "(" + COLUMN_ID + " integer primary key autoincrement, " 
+            + COLUMN_TITLE + " text not null" 
+            + COLUMN_DESCRIPTION + " text not null" 
+            + COLUMN_COUNTRY + " real not null" 
+            + COLUMN_LON + " real not null" 
+            + COLUMN_LAST_VISIT + " text" 
+            		+ ");";
+	
+	public PlacesSQLiteHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	@Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PLACES);
+        onCreate(db);
+    }
+
+}
