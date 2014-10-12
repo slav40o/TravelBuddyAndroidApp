65bcc0c6b7da3125972226bc3ace7e83d3cc607b
Slavi <slavicvetanov@gmail.com>
2014-10-12 23:04:54 +0300

    Login and register views are functuonal. Automatic login added

--- gen/com/goofy/travelbuddy/R.java
+++ gen/com/goofy/travelbuddy/R.java
@@ -35,29 +35,25 @@ public final class R {
     public static final class id {
         public static final int action_settings=0x7f08000e;
         public static final int btn_login=0x7f080001;
+        public static final int btn_register=0x7f080008;
         public static final int ed_email_input=0x7f080002;
         public static final int ed_password_input=0x7f080003;
-        public static final int fragment1=0x7f08000d;
-        public static final int placeCountry=0x7f08000a;
-        public static final int placeImage=0x7f080008;
-        public static final int placeTitle=0x7f080009;
-        public static final int placeslisttitle=0x7f08000b;
-        public static final int placeslistview=0x7f08000c;
-        public static final int textView1=0x7f080007;
+        public static final int et_confirm_pass=0x7f080009;
+        public static final int et_register_email=0x7f08000d;
+        public static final int et_register_password=0x7f08000b;
+        public static final int tv_confirm_password_label=0x7f08000a;
         public static final int tv_email_label=0x7f080005;
         public static final int tv_login_ask=0x7f080004;
         public static final int tv_password_label=0x7f080006;
+        public static final int tv_register=0x7f080007;
+        public static final int tv_register_pass_label=0x7f08000c;
         public static final int tv_welcome_message=0x7f080000;
     }
     public static final class layout {
         public static final int activity_splash_screen=0x7f030000;
         public static final int login_activity=0x7f030001;
         public static final int main_screen_activity=0x7f030002;
-        public static final int place_details=0x7f030003;
-        public static final int place_list_item=0x7f030004;
-        public static final int places_list=0x7f030005;
-        public static final int places_screen_activity=0x7f030006;
-        public static final int register_activity=0x7f030007;
+        public static final int register_activity=0x7f030003;
     }
     public static final class menu {
         public static final int splash_screen=0x7f070000;
@@ -65,11 +61,13 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int confirm_password=0x7f05000e;
         public static final int email=0x7f050009;
         public static final int hello_world=0x7f050001;
         public static final int loading=0x7f050004;
         public static final int login=0x7f05000a;
         public static final int login_ask=0x7f050006;
+        public static final int or_register=0x7f05000d;
         public static final int password=0x7f050008;
         public static final int places_title=0x7f05000c;
         public static final int register=0x7f05000b;
--- src/com/goofy/travelbuddy/LoginActivity.java
+++ src/com/goofy/travelbuddy/LoginActivity.java
@@ -1,15 +1,90 @@
 package com.goofy.travelbuddy;
 
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends Activity {
+import com.goofy.travelbuddy.connection.ClientManager;
 
+public class LoginActivity extends Activity implements View.OnClickListener{
+	private Button loginButton;
+	private TextView registerLink;
+	private EditText passwordField;
+	private EditText emailField;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login_activity);
+		initializeElements();
 	}
 
+	@Override
+	public void onClick(View v) {
+		if (v.getId() == this.loginButton.getId()) {
+			String username = this.emailField.getText().toString();
+			String password = this.passwordField.getText().toString();
+			boolean validState = true;
+			
+			if (username == null || username == "") {
+				Toast.makeText(this, "Invalid username", Toast.LENGTH_LONG).show();
+				validState = false;
+			}
+			else if(password == null || password == ""){
+				Toast.makeText(this, "Invalid password", Toast.LENGTH_LONG).show();
+				validState = false;
+			}
+			
+			if (validState) {
+				new SignUpTask().execute(username, password);
+			}
+			
+		} else if (v.getId() == this.registerLink.getId()) {
+			Intent registerIntent = new Intent(this, RegisterActivity.class);
+			startActivity(registerIntent);
+		}
+		
+	}
+
+	private void initializeElements() {
+		this.loginButton = (Button)findViewById(R.id.btn_login);
+		this.passwordField = (EditText)findViewById(R.id.ed_password_input);
+		this.emailField = (EditText)findViewById(R.id.ed_email_input);
+		this.registerLink = (TextView)findViewById(R.id.tv_register);
+		loginButton.setOnClickListener(this);
+		registerLink.setOnClickListener(this);
+	}
+	
+	private class SignUpTask extends AsyncTask<String, Void, NameValuePair> {
+		@Override
+        protected NameValuePair doInBackground(String... urls) {
+			ClientManager client = new ClientManager(getApplicationContext());
+			NameValuePair responce = client.loginUser(urls[0], urls[1]);
+			return responce;
+		}
+		
+		@Override
+        protected void onPostExecute(NameValuePair result) {
+			int status = Integer.parseInt(result.getName());
+			String message = result.getValue();
+			
+			if (status == HttpStatus.SC_OK) {
+				Intent registerIntent = new Intent(getApplicationContext(), MainActivity.class);
+				startActivity(registerIntent);
+			}
+			else{
+				Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+			}
+       }
+	}
 }
