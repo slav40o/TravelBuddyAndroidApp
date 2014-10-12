1b3ce5665fee4db264057743be445a3c19931b98
bboyadzhiev <bboyadzhiev@gmail.com>
2014-10-12 20:59:28 +0300

    Forgot the files :)

---
+++ src/com/goofy/travelbuddy/PlacesFragment.java
@@ -0,0 +1,88 @@
+package com.goofy.travelbuddy;
+
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.goofy.models.Location;
+import com.goofy.models.PlaceDetail;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Toast;
+
+public class PlacesFragment extends Fragment implements
+OnItemClickListener {
+	
+	List<PlaceDetail> places;
+	ListView placesListViews;
+	PlacesListViewAdapter placesAdapter;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.places_list, container, false);
+
+		this.places = new ArrayList<PlaceDetail>();
+		
+		// TODO Some fake data
+		addFakePlaces();
+		
+		placesListViews = (ListView) view.findViewById(R.id.placeslistview);
+		placesAdapter = new PlacesListViewAdapter(view.getContext(), R.layout.place_list_item, this.places);
+		
+		Log.d("FAKEs Added"+ places.get(0).getTitle(), " " );
+		
+		placesListViews.setAdapter(placesAdapter);
+		placesListViews.setOnItemClickListener(this);
+		
+		return view;
+	}
+	
+	 @Override
+	    public void onAttach(Activity activity) {
+	        super.onAttach(activity);
+	       
+	    }
+	
+	private void addFakePlaces() {
+		Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
+	 
+	    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+	    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+	    byte[] bitmapdata = stream.toByteArray();
+		
+		ArrayList<byte[]> photos = new ArrayList<byte[]>();
+		photos.add(bitmapdata);
+		ArrayList<String> visitors = new ArrayList<String>();
+		visitors.add("Pesho");
+		
+		places.add(new PlaceDetail(0, "Fake", "Some fake place", "JVM Land", new Location(0, 0), null, photos, visitors));
+		places.add(new PlaceDetail(0, "Fake1", "Some fake place1", "No Land", new Location(0, 0), null, photos, visitors));
+		places.add(new PlaceDetail(0, "Cool", "Some fake place2", "LapLand", new Location(0, 0), null, photos, visitors));
+		places.add(new PlaceDetail(0, "Fake3", "Some fake place3", "JVM Land", new Location(0, 0), null, photos, visitors));
+		places.add(new PlaceDetail(0, "Some Where", "Some fake place4", "JVM Land", new Location(0, 0), null, photos, visitors));
+		places.add(new PlaceDetail(0, "Fake5", "Some fake place5", "JVM Land", new Location(0, 0), null, photos, visitors));
+		places.add(new PlaceDetail(0, "Test one", "Some new fake place", "Newerland", new Location(0, 0), null, photos, visitors));
+		places.add(new PlaceDetail(0, "Another one", "Some new fake place", "Newerland", new Location(0, 0), null, photos, visitors));
+		places.add(new PlaceDetail(0, "Fake one", "Some new fake place", "Newerland", new Location(0, 0), null, photos, visitors));
+		}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		// TODO Auto-generated method stub
+		Toast.makeText(view.getContext(), "Clicked on " + places.get(position).getTitle(), Toast.LENGTH_LONG).show();
+	}
+}
---
+++ src/com/goofy/travelbuddy/PlacesListViewAdapter.java
@@ -0,0 +1,77 @@
+package com.goofy.travelbuddy;
+
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.goofy.models.PlaceDetail;
+
+public class PlacesListViewAdapter extends ArrayAdapter<PlaceDetail> {
+
+	List<PlaceDetail> places;
+	Context context;
+	int currentPosition;
+
+	public PlacesListViewAdapter(Context context, int resource,
+			List<PlaceDetail> places) {
+		super(context, resource, places);
+		this.context = context;
+		this.places = places;
+		Log.d("CREARED PlacesListViewAdapter", " " );
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		Log.d("GETVIEW PlacesListViewAdapter", " " );
+		ViewHolderItem holder;
+
+		if (convertView == null) {
+			holder = new ViewHolderItem();
+
+			LayoutInflater inflater = (LayoutInflater) context
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+			convertView = inflater.inflate(R.layout.place_list_item, parent,
+					false);
+
+			holder.placeImage = (ImageView) convertView
+					.findViewById(R.id.placeImage);
+			holder.placeTitle = (TextView) convertView
+					.findViewById(R.id.placeTitle);
+			holder.placeCountry = (TextView) convertView
+					.findViewById(R.id.placeCountry);
+
+			convertView.setTag(holder);
+			this.currentPosition = position;
+
+		} else {
+			holder = (ViewHolderItem) convertView.getTag();
+		}
+
+		// Gets the first image of them all
+		byte[] byteArray = this.places.get(position).getPhotos().get(0);
+
+		Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0,
+				byteArray.length);
+		holder.placeImage.setImageBitmap(bmp);
+		holder.placeTitle.setText(this.places.get(position).getTitle());
+		holder.placeCountry.setText(this.places.get(position).getCountry());
+
+		return convertView;
+	}
+
+	static class ViewHolderItem {
+		ImageView placeImage;
+		TextView placeTitle;
+		TextView placeCountry;
+	}
+
+}
