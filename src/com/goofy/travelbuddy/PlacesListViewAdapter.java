ddb168b3355672e9ca19717fdc26be9286ad8fe6
bboyadzhiev <bboyadzhiev@gmail.com>
2014-10-13 17:05:08 +0300

    DAO update

--- src/com/goofy/travelbuddy/PlacesListViewAdapter.java
+++ src/com/goofy/travelbuddy/PlacesListViewAdapter.java
@@ -2,6 +2,7 @@ package com.goofy.travelbuddy;
 
 import java.util.List;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -11,20 +12,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import com.goofy.models.PlaceDetail;
 
 public class PlacesListViewAdapter extends ArrayAdapter<PlaceDetail> {
 
-	List<PlaceDetail> places;
+	//List<PlaceDetail> places;
 	Context context;
 	int currentPosition;
 
-	public PlacesListViewAdapter(Context context, int resource,
-			List<PlaceDetail> places) {
+	public PlacesListViewAdapter(Context context, int resource,	List<PlaceDetail> places
+			) {
 		super(context, resource, places);
 		this.context = context;
-		this.places = places;
+		//this.places = places;
 		Log.d("CREARED PlacesListViewAdapter", " " );
 	}
 
@@ -32,7 +34,8 @@ public class PlacesListViewAdapter extends ArrayAdapter<PlaceDetail> {
 	public View getView(int position, View convertView, ViewGroup parent) {
 		Log.d("GETVIEW PlacesListViewAdapter", " " );
 		ViewHolderItem holder;
-
+		
+		
 		if (convertView == null) {
 			holder = new ViewHolderItem();
 
@@ -54,17 +57,24 @@ public class PlacesListViewAdapter extends ArrayAdapter<PlaceDetail> {
 
 		} else {
 			holder = (ViewHolderItem) convertView.getTag();
+			//View rootView = ((Activity)context).getWindow().getDecorView().findViewById(android.R.id.content);
+			//View loader = (ProgressBar) rootView.findViewById(R.id.loadingProgressBar);
+			//loader.setVisibility(View.GONE);
 		}
 
 		// Gets the first image of them all
-		byte[] byteArray = this.places.get(position).getPhotos().get(0);
+		//byte[] byteArray = this.places.get(position).getPhotos().get(0);
+		byte[] byteArray = this.getItem(position).getPhotos().get(0);
 
 		Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0,
 				byteArray.length);
 		holder.placeImage.setImageBitmap(bmp);
-		holder.placeTitle.setText(this.places.get(position).getTitle());
-		holder.placeCountry.setText(this.places.get(position).getCountry());
-
+		//holder.placeTitle.setText(this.places.get(position).getTitle());
+		//holder.placeCountry.setText(this.places.get(position).getCountry());
+		holder.placeTitle.setText(this.getItem(position).getTitle());
+		holder.placeCountry.setText(this.getItem(position).getCountry());
+		
+		
 		return convertView;
 	}
 
